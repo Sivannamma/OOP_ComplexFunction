31c3219067b03d141f13731f35969829ab2dfba4
Sivannamma <sivannamma@gmail.com>
2019-12-12 17:39:00 +0200

    finished gui test class

--- src/Ex1/Monom_Comperator.java
+++ src/Ex1/Monom_Comperator.java
@@ -4,7 +4,10 @@ import java.util.Comparator;
 
 public class Monom_Comperator implements Comparator<Monom> {
 
-	public Monom_Comperator() {;}
+	public Monom_Comperator() {
+		;
+	}
+
 	public int compare(Monom o1, Monom o2) {
 		int dp = o2.get_power() - o1.get_power();
 		return dp;
